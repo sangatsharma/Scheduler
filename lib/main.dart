10713bb24217094f99b524a3e33830a4720ce224
sangatsharma <sangatsharma2@gmail.com>
2023-09-30 16:53:43 +0545

    test

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
+      home: const ClassRoutineEntry(),
       routes: {
         SelectActor.screen: (context) => const SelectActor(),
         StudentLogin.screen: (context) => const StudentLogin(),
